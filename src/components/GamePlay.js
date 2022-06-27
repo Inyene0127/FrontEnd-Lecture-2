ae64be05a68f633f25ff9a293dbee88fa197095d
Inyene0127 <93762270+Inyene0127@users.noreply.github.com>
2022-06-27 23:33:12 +0100

    Update GamePlay.js

--- src/components/GamePlay.js
+++ src/components/GamePlay.js
@@ -1,12 +1,16 @@
-import React,{useState} from 'react'
+import React,{ useState, useEffect } from 'react'
 
 const GamePlay = (props) => { 
 
-  const [currentProblem, setCurrentProblem] = useState(generateProblem());
+  const [currentProblem, setCurrentProblem] = useState(0);
   const [userAnswer, setUserAnswer] = useState('');
-  const [prev, setPrev] = useState(1);
+  const [gameCount, setgameCount] = useState(1);
 
-
+    useEffect(() => {
+      setCurrentProblem(generateProblem());
+      //  let correctAnswer;
+    }, []);
+ 
   function generateNumber(max) {
     return Math.floor(Math.random() * max)
   }
@@ -15,51 +19,62 @@ const GamePlay = (props) => {
     return {
       numberOne: generateNumber(10),
       numberTwo: generateNumber(10),
-      operator: ['+', 'x'][generateNumber(2)] //incase I want to add more operators.
+      operator: ['+', 'x', '-'][generateNumber(3)]//incase I want to add more operators.
     }
   }
 
-  let correctAnswer;
-  if (currentProblem.operator == "+") {
-    correctAnswer = currentProblem.numberOne + currentProblem.numberTwo
-  }
-  else if (currentProblem.operator == "x") {
-    correctAnswer = currentProblem.numberOne * currentProblem.numberTwo
-  };
 
     const Submit = (event) => {
       event.preventDefault();
-      const validAnswer = userAnswer >= 0;
-          if (!validAnswer) {
-            alert('Input a positive number please') ;}
-            if (correctAnswer == parseInt(userAnswer)) {
-              setPrev((prev) => prev + 1);
-              if (prev == props.round) {
-                props.setPlay(2);
+
+      let correctAnswer;
+
+      if (currentProblem.operator == "+") {
+        
+        correctAnswer = currentProblem.numberOne + currentProblem.numberTwo;
+        console.log(correctAnswer);
+      }
+          else if (currentProblem.operator == "x") {
+      
+          correctAnswer = currentProblem.numberOne * currentProblem.numberTwo;
+          console.log(correctAnswer);
+        }
+        //   else if (currentProblem.operator == "/") {
+        //     setCorrectAnswer(currentProblem.numberOne / currentProblem.numberTwo)
+        // }
+          else if (currentProblem.operator == "-") {
+            
+          correctAnswer = currentProblem.numberOne - currentProblem.numberTwo;
+           console.log(correctAnswer);
+        }
+
+      const validAnswer = parseInt(userAnswer);
+
+          if (typeof (validAnswer) !== 'number'){
+            alert('Input a positive number please'); 
+          }
+          if (correctAnswer == parseInt(userAnswer)) {
+              setgameCount((gameCount) => gameCount + 1);
+          if (gameCount == props.round) {
+                props.setGameMode('End Game');
               }
               setUserAnswer("");
               setCurrentProblem(generateProblem());
+              correctAnswer;
+              console.log(correctAnswer);
             }
-              
-              
-            
-          
         } 
 
   
 
   return (
-    <div>
-        <div id='second_container' className='second_header'>
-          <div>
-            <form onSubmit={Submit}>
-      <h1 id='numbers'>{`${currentProblem.numberOne} ${currentProblem.operator} ${currentProblem.numberTwo}`}</h1>
-      <input id="inputAns" value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)}/>
-      <button id='btn_1' type='submit'>Submit</button>
-      </form>
-          </div>
-        </div>
-    </div>
+          <div id='container' className='header'>
+            <h1>{`${currentProblem.numberOne} ${currentProblem.operator} ${currentProblem.numberTwo}`}</h1>
+              <form onSubmit={Submit}>
+                <input className="inputVal" value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)}/>
+                  <button id='btn' type='submit' >Submit</button>
+              </form>
+        </div>//reduce the number of divs used.
   )
 }
 
