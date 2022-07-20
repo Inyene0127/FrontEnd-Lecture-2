0f572f9d6f0be858c8704cafad06ace7e30e3dcb
Inyene0127 <93762270+Inyene0127@users.noreply.github.com>
2022-07-20 12:03:39 +0100

    Add files via upload

---
+++ src/components/SkipButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SkipButton = ({onClick}) => {
+
+    
+  
+
+
+
+  return (
+    <div>
+      <button id='btn' onClick={onClick}>Skip</button>
+    </div>
+  )
+}
+
+export default SkipButton
