0bfea80b8cf63b301321ed0ad857ec247a920c16
Inyene0127 <93762270+Inyene0127@users.noreply.github.com>
2022-07-22 15:04:02 +0100

    Add files via upload

---
+++ src/hooks/Reducer.js
@@ -0,0 +1,57 @@
+export const changeGameMode = (newGameMode) => ({
+      type: "changeGameMode",
+      payload: newGameMode
+});
+
+export const changeRound = (newRound) => ({
+  type: 'changeRound',
+  payload: newRound
+});
+
+export const startTimer = () => ({
+  type: 'startTimer'
+});
+
+// export const changeGameHistory = (newGameHistory) => ({
+//   type: 'changeGameHistory',
+//   payload: newGameHistory
+// });
+
+// export const changePlayedRounds = (newPlayedRounds) => ({
+//   type: 'changePlayedRounds',
+//   payload: newPlayedRounds
+// });
+
+export const initializer = () => ({
+  round: 3,
+  count: 1,
+  time: null,
+});
+
+const setRound = (state, newRound) => ({
+  ...state,
+  round: newRound
+});
+
+const setGameMode = (state, newGameMode) => ({
+  ...state,
+  gameMode: newGameMode
+});
+
+const setTimer = (state) => ({
+  ...state,
+  timer: Date.now()
+})
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case 'changeGameMode':
+      return setGameMode(state, action.payload);
+    case 'changeRound':
+      return setRound(state, action.payload);
+    case 'startTimer':
+        return setTimer(state, action.payload);
+    default:
+      throw new Error("Invalid");
+  }
+}
